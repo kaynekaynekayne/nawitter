ae5c93c7d3581b34705a20e50d8f6098905eb245
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-06-28 01:14:12 +0900

    change input ui

--- src/components/nawit_add/nawit_add.jsx
+++ src/components/nawit_add/nawit_add.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './nawit_add.module.css';
 
 const NawitAdd = () => {
 
+    const inputRef=useRef();
+    
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        const nawit=inputRef.current.value;
+        console.log(nawit);
+
+        try{
+            
+        } catch{
+            
+        };
+        
+        inputRef.current.value="";
+    }
 
     return(
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={onSubmit}>
             <div className={styles.container}>
                 <input
+                    ref={inputRef}
                     className={styles.input} 
                     type="text"
                     placeholder="say everything you want"
