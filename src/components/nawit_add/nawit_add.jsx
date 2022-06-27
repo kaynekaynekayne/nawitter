fee3181667ed2f6e516cac3d7a5a7994fe59ffbe
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-06-28 00:38:48 +0900

    making writing input ui

--- src/components/nawit_add/nawit_add.jsx
+++ src/components/nawit_add/nawit_add.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import styles from './nawit.module.css';
+import styles from './nawit_add.module.css';
 
 const NawitAdd = () => {
 
 
     return(
+        <form className={styles.form}>
+            <div className={styles.container}>
+                <input
+                    className={styles.input} 
+                    type="text"
+                    placeholder="say everything you want"
+                    maxLength="120"
+                    // value={}
+                    autoFocus
+                />
+                <label htmlFor="file-upload" className={styles.chooseImg}>
+                    <i className="fas fa-image"></i>
+                </label>
+                <input 
+                    id="file-upload"
+                    type="file"
+                    style={{display:"none"}}
+                />
+                <button className={styles.submit} type="submit">
+                    <i className="fas fa-pen"></i>
+                </button>
+            </div>
 
+        </form>
     )
 };
 
--- src/components/nawitter-home/home.jsx
+++ src/components/nawitter-home/home.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import styles from './home.module.css';
-import Header from '../header/header';
+import NawitAdd from '../nawit_add/nawit_add';
 
 const Home = ({userObj}) => {
     
     return(
         <section className={styles.home}>
-            {/* <Header /> */}
-            <div className={styles.container}>
-                <h1>home</h1>
-                <h2>{userObj.userName}</h2>
-                <h3>{userObj.userEmail}</h3>
-                <h3>{userObj.userId}</h3>
-            </div>
+            <NawitAdd userObj={userObj} />
         </section>
     )
 };
