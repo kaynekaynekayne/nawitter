a23310df7347be9bb69d52c57e0dd0067c8c1dfb
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-06-29 16:09:31 +0900

    making nawit entire frame

--- src/components/nawit/nawit.jsx
+++ src/components/nawit/nawit.jsx
@@ -5,24 +5,24 @@ const Nawit = ({nawit, isOwner}) => {
 
     return(
         <section className={styles.nawit}>
-            <div className={styles.contentBox}>
-                <p className={styles.content}>{nawit.content}</p>
-            </div>
-            <div className={styles.bottomBox}>
-                <p className={styles.nickname}>@{nawit.creatorName}</p>
-                {isOwner && (
-                    <div className={styles.buttons}>
-                        <button 
-                            className={`${styles.edit} ${styles.button}`}
-                            >수정
-                        </button>
-                        <button 
-                            className={`${styles.delete} ${styles.button}`}
-                            >삭제
-                        </button>
-                    </div>
-                )}
-            </div>
+                <div className={styles.contentBox}>
+                    <p className={styles.content}>{nawit.content}</p>
+                </div>
+                <div className={styles.bottomBox}>
+                    <p className={styles.nickname}>@{nawit.creatorName}</p>
+                    {isOwner && (
+                        <div className={styles.buttons}>
+                            <button 
+                                className={`${styles.edit} ${styles.button}`}
+                                >수정
+                            </button>
+                            <button 
+                                className={`${styles.delete} ${styles.button}`}
+                                >삭제
+                            </button>
+                        </div>
+                    )}
+                </div>
         </section>
     )
 };
