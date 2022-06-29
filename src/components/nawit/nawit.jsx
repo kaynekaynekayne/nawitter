656d0482f23bb4e1d4030244f5c14a6b5c0ab0f6
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-06-30 00:15:08 +0900

    making togge button for editing content

--- src/components/nawit/nawit.jsx
+++ src/components/nawit/nawit.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './nawit.module.css';
 
 const Nawit = ({nawit, isOwner, nawitService}) => {
+
+    const [editing,setEditing]=useState(false);
+    const [newContent,setNewContent]=useState(nawit.content);
 
     const deletePost=async(id)=>{
         await nawitService.deleteContent(id);
     }
 
+    const toggleEdit=()=>setEditing((prev)=>!prev);
+
     return(
         <section className={styles.nawit}>
-                <div className={styles.contentBox}>
-                    <p className={styles.content}>{nawit.content}</p>
-                </div>
-                <div className={styles.bottomBox}>
-                    <p className={styles.nickname}>@{nawit.creatorName}</p>
-                    {isOwner && (
-                        <div className={styles.buttons}>
-                            <button 
-                                className={`${styles.edit} ${styles.button}`}
-                                >수정
-                            </button>
-                            <button 
-                                className={`${styles.delete} ${styles.button}`}
-                                onClick={()=>deletePost(nawit.id)}
-                                >삭제
-                            </button>
-                        </div>
-                    )}
-                </div>
+            {editing ? (
+                <>
+                
+                </>
+            ) : (
+                <>
+                    <div className={styles.contentBox}>
+                        <p className={styles.content}>{nawit.content}</p>
+                    </div>
+                    <div className={styles.bottomBox}>
+                        <p className={styles.nickname}>@{nawit.creatorName}</p>
+                        {isOwner && (
+                            <div className={styles.buttons}>
+                                <button 
+                                    className={`${styles.edit} ${styles.button}`}
+                                    onClick={toggleEdit}
+                                    >수정
+                                </button>
+                                <button 
+                                    className={`${styles.delete} ${styles.button}`}
+                                    onClick={()=>deletePost(nawit.id)}
+                                    >삭제
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                </>
+            )}
         </section>
     )
 };
